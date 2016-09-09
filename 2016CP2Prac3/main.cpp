704eb2895e5fee3ecf112d541255cf415d721eea
k <k@debian>
2016-09-09 13:46:53 +0930

    Finished all levels.

--- 2016CP2Prac3/main.cpp
+++ 2016CP2Prac3/main.cpp
@@ -15,6 +15,9 @@
 #include <sstream>
 #include <iostream>
 #include "HPStack.h"
+#include <string>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -23,15 +26,101 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     HPStack stack;
+    HPStack mem;
     string line;
     while (getline(cin, line)) {
         stringstream expression(line);
         string token;
         while (expression >> token) {
+            transform(token.begin(), token.end(), token.begin(), ::tolower);
             if (isdigit(token[0])) {
                 //cout << "that's a number" << endl;
                 stack.push(atof(token.data()));
-            } else if (token == "+") { //other arithmetic ops similar
+            } else if (token == "pi") {
+                stack.push(M_PI);
+            } else if (token == "chs") {
+                double x = stack.pop();
+                x *= -1;
+                stack.push(x);
+            } else if (token == "recip") {
+                double x = stack.pop();
+                x = 1 / x;
+                stack.push(x);
+            } else if (token == "log") {
+                double x = stack.pop();
+                x = log10(x);
+                stack.push(x);
+            } else if (token == "ln") {
+                double x = stack.pop();
+                x = log(x);
+                stack.push(x);
+            } else if (token == "exp") {
+                double x = stack.pop();
+                x = exp(x);
+                stack.push(x);
+            } else if (token == "sqrt") {
+                double x = stack.pop();
+                x = sqrt(x);
+                stack.push(x);
+            } else if (token == "sin") {
+                double x = stack.pop();
+                x = sin(x);
+                stack.push(x);
+            } else if (token == "cos") {
+                double x = stack.pop();
+                x = cos(x);
+                stack.push(x);
+            } else if (token == "tan") {
+                double x = stack.pop();
+                x = tan(x);
+                stack.push(x);
+            } else if (token == "arcsin") {
+                double x = stack.pop();
+                x = asin(x);
+                stack.push(x);
+            } else if (token == "arccos") {
+                double x = stack.pop();
+                x = acos(x);
+                stack.push(x);
+            } else if (token == "arctan") {
+                double x = stack.pop();
+                x = atan(x);
+                stack.push(x);
+            } else if (token == "pow") {
+                double x = stack.pop();
+                double y = stack.pop();
+                x = pow(x, y);
+                stack.push(x);
+            } else if (token == "sto") {
+                mem.push(stack.peek());
+            } else if (token == "rcl") {
+                stack.push(mem.pop());
+            } else if (token == "clr") {
+                for (int i = 0; i < 4; i++) {
+                    stack.push(0.0);
+                }
+            } else if (token == "clx") {
+                double x = stack.pop();
+            } 
+            else if (token == "swap"){
+                double x = stack.pop();
+                double y = stack.pop();
+                stack.push(x);
+                stack.push(y);
+            }
+            else if(token == "roll"){
+                double x = stack.pop();
+                double y = stack.pop();
+                double z = stack.pop();
+                double t = stack.pop();
+                stack.push(x);
+                stack.push(t);
+                stack.push(z);
+                stack.push(y);
+            }else if (token == "enter"){
+                stack.push(stack.peek());
+            }
+            else if (token == "+") { //other arithmetic ops similar
                 //cout << "you pressed +" << endl;
                 //stack.show();
                 double x = stack.pop();
@@ -55,8 +144,9 @@ int main(int argc, char** argv) {
                 double y = stack.pop();
                 stack.push(y / x);
             }
-            stack.show();
+            //stack.show();
         }
+        cout << stack.peek() << endl;
 
 
     }
